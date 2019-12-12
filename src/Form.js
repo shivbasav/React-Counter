f25e16f74d1e62500f2ea83c1e5a73e4e6067c14
Shivbasav Nandargi <shivagn119@gmail.com>
2019-12-12 22:36:21 +0530

    configuration

---
+++ src/Form.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: "1rem";
+`;
+
+const Form = () => {
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values, { setSubmitting }) => console.log("Submitting")}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit
+        } = props;
+        return (
+          <Container>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="e.g. abc@example.com"
+                />
+              </div>
+            </div>
+          </Container>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default Form;
